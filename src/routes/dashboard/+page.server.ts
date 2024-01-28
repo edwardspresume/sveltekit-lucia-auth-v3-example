106e062a47c03681f17829652214a4ad15dd0459
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 14:13:36 -0500

    Add database utility functions and update page server load
    This commit adds two new functions, `deleteAllUsers` and `getAllUsers`, to the database utility module. It also updates the page server load function to include the `allUsers` property in the returned object. These changes enhance the functionality of the dashboard page.

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -4,6 +4,7 @@ import { redirect } from 'sveltekit-flash-message/server';
 
 import { route } from '$lib/ROUTES';
 import { lucia } from '$lib/database/auth.server';
+import { deleteAllUsers, getAllUsers } from '$lib/database/databaseUtils.server';
 
 export const load = (async ({ locals: { user }, cookies }) => {
 	if (!user) {
@@ -18,7 +19,8 @@ export const load = (async ({ locals: { user }, cookies }) => {
 	}
 
 	return {
-		loggedInUserName: user.name
+		loggedInUserName: user.name,
+		allUsers: await getAllUsers()
 	};
 }) satisfies PageServerLoad;
 
@@ -34,6 +36,25 @@ export const actions: Actions = {
 			path: '.',
 			...sessionCookie.attributes
 		});
+
+		throw redirect(303, route('/auth/login'));
+	},
+
+	deleteAllUsers: async ({ locals, cookies }) => {
+		if (!locals.session?.id) return;
+
+		await lucia.invalidateSession(locals.session.id);
+
+		await lucia.invalidateUserSessions(locals.session.userId);
+
+		const sessionCookie = lucia.createBlankSessionCookie();
+
+		cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+
+		await deleteAllUsers();
 
 		throw redirect(303, route('/auth/login'));
 	}
