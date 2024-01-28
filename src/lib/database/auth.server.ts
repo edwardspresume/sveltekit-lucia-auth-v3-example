eccbef9648a0f03c6963b604eaac051b9a93f89b
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 09:47:57 -0500

    feat(auth.server.ts): add authentication server module for handling user authentication and session management

---
+++ src/lib/database/auth.server.ts
@@ -0,0 +1,34 @@
+import { dev } from '$app/environment';
+
+import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
+import { Lucia } from 'lucia';
+
+import { database } from './database.server';
+import { usersSessionsTable, usersTable } from './schema';
+
+const dbAdapter = new DrizzleSQLiteAdapter(database, usersSessionsTable, usersTable);
+
+export const lucia = new Lucia(dbAdapter, {
+	sessionCookie: {
+		attributes: {
+			secure: !dev
+		}
+	},
+
+	getUserAttributes: (attributes) => {
+		return {
+			email: attributes.email
+		};
+	}
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+		DatabaseUserAttributes: DatabaseUserAttributes;
+	}
+}
+
+type DatabaseUserAttributes = {
+	email: string;
+};
