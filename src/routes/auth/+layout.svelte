a0ef0d5040704ef3627715aac54afe3df967c983
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 11:14:10 -0500

    Refactor auth layout styles

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -6,8 +6,8 @@
 	import authImg from '$lib/assets/auth_image.jpg?enhanced';
 </script>
 
-<div class="md:grid md:grid-cols-[1fr,1.6fr] md:gap-10">
-	<enhanced:img src={authImg} alt="An alt text" class="hidden md:block" />
+<div class="md:grid md:grid-cols-[1fr,1.6fr] md:gap-8">
+	<enhanced:img src={authImg} alt="An alt text" class="hidden rounded shadow-md md:block" />
 
 	<section>
 		<slot />
