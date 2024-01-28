d72976a033a1271321ade2928ca3f8064d06ca19
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 14:13:42 -0500

    Update dashboard page layout and add user list functionality

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -8,10 +8,34 @@
 	export let data: PageData;
 </script>
 
-<h1 class="mb-5 text-2xl">
-	Hello <span class="font-bold">{data.loggedInUserName}</span>
-</h1>
+<div>
+	<section>
+		<h1 class="mb-5 text-2xl">
+			Hello <span class="font-bold">{data.loggedInUserName}</span>
+		</h1>
 
-<form method="post" action={route('logout /dashboard')}>
-	<SubmitButton>Logout</SubmitButton>
-</form>
+		<form method="post" action={route('logout /dashboard')}>
+			<SubmitButton>Logout</SubmitButton>
+		</form>
+	</section>
+
+	<hr class="my-8" />
+
+	<section class="max-w-md mt-4">
+		<header class="flex flex-wrap items-center justify-between gap-3 mb-9">
+			<h2 class="text-xl font-bold leading-none">List of all register users</h2>
+
+			<form method="post" action={route('deleteAllUsers /dashboard')}>
+				<SubmitButton>Delete all users</SubmitButton>
+			</form>
+		</header>
+
+		<ul class="space-y-4">
+			{#each data.allUsers as user}
+				<li class="p-2 rounded bg-accent">
+					{user.name} - {user.email}
+				</li>
+			{/each}
+		</ul>
+	</section>
+</div>
