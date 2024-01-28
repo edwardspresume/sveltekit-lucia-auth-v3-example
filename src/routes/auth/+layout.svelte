b33541ccbdfb42cdd2c8017534be197edd31bbc9
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 11:00:08 -0500

    feat(auth): add layout component for authentication routes
    This commit adds a new layout component for the authentication routes. The layout component is responsible for rendering the authentication pages with a consistent layout. It includes a grid layout with two columns, where the first column displays an enhanced image and the second column contains the content of the authentication page.
    
    The layout component imports an image asset `auth_image.jpg` from the `src/lib/assets` directory. The image is enhanced using a build tool. The enhanced image is then used as the source for an `<img>` element in the layout component.
    
    This layout component improves the user experience by providing a visually appealing layout for the authentication pages.

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	import authImg from '$lib/assets/auth_image.jpg?enhanced';
+</script>
+
+<div class="md:grid md:grid-cols-[1fr,1.6fr] md:gap-10">
+	<enhanced:img src={authImg} alt="An alt text" class="hidden md:block" />
+
+	<section>
+		<slot />
+	</section>
+</div>
