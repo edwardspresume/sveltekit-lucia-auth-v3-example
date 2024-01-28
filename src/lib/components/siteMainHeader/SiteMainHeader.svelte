88226bfd417957a79dee7ef1137c67e7fdf54978
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 11:02:05 -0500

    Update site main header logo text

--- src/lib/components/siteMainHeader/SiteMainHeader.svelte
+++ src/lib/components/siteMainHeader/SiteMainHeader.svelte
@@ -9,7 +9,9 @@
 
 <header class="relative mb-10 border-b">
 	<div class="container flex items-center justify-between p-2">
-		<Button href={route('/')} variant="link" class="p-0 text-2xl font-bold">Logo</Button>
+		<Button href={route('/')} variant="link" class="p-0 text-2xl font-bold"
+			>Lucia Auth Example</Button
+		>
 
 		<div class="flex gap-2">
 			<SiteMainNav />
