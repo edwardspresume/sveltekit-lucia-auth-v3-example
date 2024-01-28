f921435e4e1116386039f119d06fad3f87fec81a
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 10:05:51 -0500

    feat(database): Migration: add new table `users_sessions` and foreign key constraint to `users` table

---
+++ drizzle/0001_chilly_frog_thor.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users_sessions` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
