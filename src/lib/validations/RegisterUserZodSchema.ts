22f44659cd32bec5fba29c63cf2b104fbe2b89aa
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 09:52:31 -0500

    Add constants for minimum name and password length in RegisterUserZodSchema

--- src/lib/validations/RegisterUserZodSchema.ts
+++ src/lib/validations/RegisterUserZodSchema.ts
@@ -1,13 +1,21 @@
 import { usersTable } from '$lib/database/schema';
 import { createInsertSchema } from 'drizzle-zod';
 
+export const MIN_NAME_LENGTH = 2;
 export const MAX_NAME_LENGTH = 50;
+export const MIN_PASSWORD_LENGTH = 2;
 
 export const RegisterUserZodSchema = createInsertSchema(usersTable, {
 	name: (schema) =>
 		schema.name
-			.min(2, 'Name must be at least 2 characters long')
+			.min(MIN_NAME_LENGTH, `Name must be at least ${MIN_NAME_LENGTH} characters long`)
 			.max(MAX_NAME_LENGTH, `Name must be less than ${MAX_NAME_LENGTH} characters long`),
+
 	email: (schema) => schema.email.email(),
-	password: (schema) => schema.password.min(2, 'Password must be at least 2 characters long')
+
+	password: (schema) =>
+		schema.password.min(
+			MIN_PASSWORD_LENGTH,
+			`Password must be at least ${MIN_PASSWORD_LENGTH} characters long`
+		)
 });
