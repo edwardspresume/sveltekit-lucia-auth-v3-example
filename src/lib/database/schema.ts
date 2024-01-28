d8e26c4d993d887746447159b5950feec42f29e4
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 09:41:53 -0500

    Add usersSessionsTable to database schema

--- src/lib/database/schema.ts
+++ src/lib/database/schema.ts
@@ -1,14 +1,26 @@
 import { sql } from 'drizzle-orm';
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const usersTable = sqliteTable('users', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
+	id: text('id').primaryKey().notNull(),
+
 	name: text('name').notNull(),
+
 	email: text('email').notNull().unique(),
+
 	password: text('password').notNull(),
+
 	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
+});
+
+export const usersSessionsTable = sqliteTable('users_sessions', {
+	id: text('id').primaryKey().notNull(),
+
+	userId: text('user_id')
+		.notNull()
+		.references(() => usersTable.id),
+
+	expiresAt: integer('expires_at').notNull()
 });
 
 export type UserInsertSchema = typeof usersTable.$inferInsert;
