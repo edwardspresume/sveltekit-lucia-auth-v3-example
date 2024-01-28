82e4c67440c57b67a3e48c940fd487af304c0a9a
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 10:59:58 -0500

    removed unneeded code for that page

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -33,59 +33,33 @@
 	});
 </script>
 
-<div class="space-y-4 sm:grid sm:grid-cols-2 sm:justify-between sm:gap-10">
-	<section>
-		<h2 class="mb-4 text-2xl font-bold">Register</h2>
+<h1 class="mb-4 text-2xl font-bold">Register</h1>
 
-		<form method="post" use:enhance class="space-y-4" action={route('registerUser /auth/register')}>
-			<InputField
-				type="text"
-				name="name"
-				label="Name"
-				bind:value={$form.name}
-				errorMessage={$errors.name}
-				maxlength={MAX_NAME_LENGTH}
-			/>
+<form method="post" use:enhance class="space-y-4" action={route('registerUser /auth/register')}>
+	<InputField
+		type="text"
+		name="name"
+		label="Name"
+		bind:value={$form.name}
+		errorMessage={$errors.name}
+		maxlength={MAX_NAME_LENGTH}
+	/>
 
-			<InputField
-				type="email"
-				name="email"
-				label="Email"
-				bind:value={$form.email}
-				errorMessage={$errors.email}
-			/>
+	<InputField
+		type="email"
+		name="email"
+		label="Email"
+		bind:value={$form.email}
+		errorMessage={$errors.email}
+	/>
 
-			<InputField
-				type="password"
-				name="password"
-				label="Password"
-				bind:value={$form.password}
-				errorMessage={$errors.password}
-			/>
+	<InputField
+		type="password"
+		name="password"
+		label="Password"
+		bind:value={$form.password}
+		errorMessage={$errors.password}
+	/>
 
-			<SubmitButton />
-		</form>
-	</section>
-
-	<hr class="sm:hidden" />
-
-	<section>
-		<header class="flex flex-wrap justify-between gap-3">
-			<h2 class="mb-4 text-2xl font-bold">Registered users</h2>
-
-			<form method="post" action={route('deleteAllUsers /auth/register')}>
-				<SubmitButton>Delete all users</SubmitButton>
-			</form>
-		</header>
-
-		{#if data.allUsers}
-			<ul class="mt-4 space-y-4">
-				{#each data.allUsers as user}
-					<li class="p-2 rounded bg-accent">
-						{user.name} - {user.email}
-					</li>
-				{/each}
-			</ul>
-		{/if}
-	</section>
-</div>
+	<SubmitButton />
+</form>
