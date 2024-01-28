b3bec541ea1f0a707ded1ee7a14aeb613da64bdc
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 12:20:45 -0500

    used name form action for better clarity

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import { route } from '$lib/ROUTES';
+
 	import SubmitButton from '$components/form/SubmitButton.svelte';
 
 	export let data: PageData;
@@ -10,6 +12,6 @@
 	Hello <span class="font-bold">{data.loggedOnUserName}</span>
 </h1>
 
-<form method="post">
+<form method="post" action={route('logout /dashboard')}>
 	<SubmitButton>Logout</SubmitButton>
 </form>
