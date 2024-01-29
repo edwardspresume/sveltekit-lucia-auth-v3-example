d93a2bac6cab21d9df4722a8f42b44322dee65d8
Edwards Presume <edwardspresume@gmail.com>
2024-01-28 19:47:05 -0500

    moved redirection logic

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,20 +4,20 @@ import { lucia } from '$lib/database/auth.server';
 import { DASHBOARD_ROUTE } from '$lib/utils/navLinks';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	// Get the session ID from the cookies
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 
+	// If there's no session ID, set user and session to null and resolve the request
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
 		return resolve(event);
 	}
 
+	// If there's a session ID, validate it
 	const { session, user } = await lucia.validateSession(sessionId);
 
-	if (session && !event.url.pathname.startsWith(DASHBOARD_ROUTE) && event.url.pathname !== '/') {
-		throw redirect(303, DASHBOARD_ROUTE);
-	}
-
+	// If the session is fresh (just created due to session expiration extending), create a new session cookie
 	if (session?.fresh) {
 		const sessionCookie = lucia.createSessionCookie(session.id);
 
@@ -27,6 +27,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		});
 	}
 
+	// If the session is not valid, create a blank session cookie to delete a session cookie from the browser
 	if (!session) {
 		const sessionCookie = lucia.createBlankSessionCookie();
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
@@ -35,6 +36,12 @@ export const handle: Handle = async ({ event, resolve }) => {
 		});
 	}
 
+	// Redirect to the dashboard only if the session is valid and the request isn't for the dashboard or the home page
+	if (event.url.pathname !== DASHBOARD_ROUTE && event.url.pathname !== '/') {
+		throw redirect(303, DASHBOARD_ROUTE);
+	}
+
+	// Store the user and session in event.locals, so they can be accessed in endpoints and pages
 	event.locals.user = user;
 	event.locals.session = session;
 
