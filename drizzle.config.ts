0f3d202104a7dc812b6621151795fe54feb99b1f
Edwards Presume <edwardspresume@gmail.com>
2024-02-03 08:55:09 -0500

    Update drizzle.config.ts to use DATABASE_URL from environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,20 @@
+import * as dotenv from 'dotenv';
 import type { Config } from 'drizzle-kit';
+
+dotenv.config();
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+	throw new Error(
+		'No DATABASE_URL defined in the environment variables. Please ensure it is set in the .env file.'
+	);
+}
 
 export default {
 	schema: 'src/lib/database/schema.ts',
 	out: './drizzleMigrations',
 	driver: 'better-sqlite',
 	dbCredentials: {
-		url: 'localDB/sqlite.db'
+		url: DATABASE_URL
 	}
 } satisfies Config;
