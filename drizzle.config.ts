ae6d3bdafc972dc603c4ad64daf7df9ebffc9f67
Edwards Presume <edwardspresume@gmail.com>
2024-02-03 08:46:17 -0500

    renamed drizzle migration folder to be more clear

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export default {
 	schema: 'src/lib/database/schema.ts',
-	out: './drizzle',
+	out: './drizzleMigrations',
 	driver: 'better-sqlite',
 	dbCredentials: {
 		url: 'localDB/sqlite.db'
