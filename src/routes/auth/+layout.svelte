881c74a561065c7833c2132012f2e39f143a3017
Edwards Presume <edwardspresume@gmail.com>
2024-02-12 08:58:43 -0500

    feat(auth): add images for login and register pages to improve visual appeal and user experience feat(auth): update image source based on current page URL to display the appropriate image for login and register pages dynamically

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-	import authImg from '$lib/assets/auth_image.jpg?enhanced';
+	import { page } from '$app/stores';
+	import { route } from '$lib/ROUTES';
+
+	import login_page from '$lib/assets/login_page.jpg?enhanced';
+	import register_page from '$lib/assets/register_page.jpg?enhanced';
+
+	const images = {
+		[route('/auth/register')]: register_page,
+		[route('/auth/login')]: login_page
+	};
+
+	$: currentPageImage = images[$page.url.pathname];
 </script>
 
 <div class="md:grid md:grid-cols-[1fr,1.6fr] md:gap-8">
-	<enhanced:img src={authImg} alt="An alt text" class="hidden rounded shadow-md md:block" />
+	<enhanced:img
+		alt="An alt text"
+		src={currentPageImage}
+		class="hidden rounded shadow-md md:block"
+	/>
 
 	<section>
 		<slot />
