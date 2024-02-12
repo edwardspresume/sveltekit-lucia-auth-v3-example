3170bbea3dade248f11d76c5ba353ff533a53ab3
Edwards Presume <edwardspresume@gmail.com>
2024-02-12 14:50:14 -0500

    Add deleteSessionCookie function to authUtils.server.ts

---
+++ src/lib/database/authUtils.server.ts
@@ -0,0 +1,11 @@
+import type { Cookies } from '@sveltejs/kit';
+import type { Lucia } from 'lucia';
+
+export const deleteSessionCookie = async (lucia: Lucia, cookies: Cookies) => {
+	const sessionCookie = lucia.createBlankSessionCookie();
+
+	cookies.set(sessionCookie.name, sessionCookie.value, {
+		path: '.',
+		...sessionCookie.attributes
+	});
+};
