d3860a6c055323cc328c39b03b6c755a1370497d
Edwards Presume <edwardspresume@gmail.com>
2024-02-12 12:38:41 -0500

    refactor(hooks.server.ts): improve comments for better code readability and maintainability

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,51 +1,50 @@
 import { redirect, type Handle } from '@sveltejs/kit';
 
-import { route } from '$lib/ROUTES';
 import { lucia } from '$lib/database/auth.server';
-import { DASHBOARD_ROUTE } from '$lib/utils/navLinks';
+import { AUTH_ROUTES, DASHBOARD_ROUTE } from '$lib/utils/navLinks';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// Get the session ID from the cookies
+	// Retrieve the session ID from the browser's cookies
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 
-	// If there's no session ID, set user and session to null and resolve the request
+	// If there's no session ID, set both user and session to null and resolve the request
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
 		return resolve(event);
 	}
 
-	// If there's a session ID, validate it
+	// Attempt to validate the session using the retrieved session ID
 	const { session, user } = await lucia.validateSession(sessionId);
 
-	// If the session is fresh (just created due to session expiration extending), create a new session cookie
+	// If the session is newly created (due to session expiration extension), generate a new session cookie
 	if (session?.fresh) {
 		const sessionCookie = lucia.createSessionCookie(session.id);
 
+		// Set the new session cookie in the browser
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes
 		});
 	}
 
-	// If the session is not valid, create a blank session cookie to delete a session cookie from the browser
+	// If the session is invalid, generate a blank session cookie to remove the existing session cookie from the browser
 	if (!session) {
 		const sessionCookie = lucia.createBlankSessionCookie();
+
+		// Set the blank session cookie in the browser
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes
 		});
 	}
 
-	// List of AUTH routes
-	const AUTH_ROUTES = [route('/auth/login'), route('/auth/register')];
-
-	// If a logged-in user tries to access the login or register page, redirect them to the dashboard.
+	// If a user is logged in and attempts to access the login or register page, redirect them to the dashboard
 	if (session && AUTH_ROUTES.includes(event.url.pathname)) {
 		throw redirect(303, DASHBOARD_ROUTE);
 	}
 
-	// Store the user and session in event.locals, so they can be accessed in endpoints and pages
+	// Persist the user and session information in the event locals for use within endpoint handlers and page components
 	event.locals.user = user;
 	event.locals.session = session;
 
