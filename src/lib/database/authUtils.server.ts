527aba01a172c2a5ce45348fab029c4b85261105
Edwards Presume <edwardspresume@gmail.com>
2024-02-19 10:33:22 -0500

    Refactor email sending functions

--- src/lib/database/authUtils.server.ts
+++ src/lib/database/authUtils.server.ts
@@ -21,6 +21,12 @@ export type PendingVerificationUserDataType = {
 	email: string;
 };
 
+type EmailParams = {
+	email: string;
+	subject: string;
+	htmlContent: string;
+};
+
 function createRateLimiter(cookieName: string, cookieSecret: string) {
 	return new RetryAfterRateLimiter({
 		// A rate is defined as [number, unit]
@@ -74,6 +80,63 @@ export const deleteSessionCookie = async (lucia: Lucia, cookies: Cookies) => {
 	});
 };
 
+const sendEmail = async ({ email, subject, htmlContent }: EmailParams) => {
+	const { error } = await resend.emails.send({
+		from: 'Lucia V3 example <onboarding@resend.dev>',
+		to: [email],
+		subject,
+		html: htmlContent
+	});
+
+	if (error) {
+		console.error({ error });
+		return { success: false, message: `Failed to send email: ${error.message}` };
+	}
+
+	return {
+		success: true,
+		message: `An email has been sent to ${email} with the subject: ${subject}.`
+	};
+};
+
+export const sendEmailVerificationCode = async (email: string, code: string) => {
+	const htmlContent = `
+	<div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+		<h1>Email Verification</h1>
+		<p>Thank you for taking the time to verify your email address. Your verification code is:</p>
+		<p style="font-size: 20px;"><strong>${code}</strong></p>
+		<p>Please enter this code in the verification field to complete the process. If you did not request this verification, please ignore this email.</p>
+	</div>
+	`;
+
+	return sendEmail({
+		email,
+		subject: 'Email Verification Code Request',
+		htmlContent
+	});
+};
+
+export const sendPasswordResetEmail = async (email: string, resetToken: string) => {
+	const htmlContent = `
+	<div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+		<h1>Password Reset Request</h1>
+		<p>We've received a request to reset your password. If you didn't make the request, just ignore this email. Otherwise, you can reset your password using the link below.</p>
+
+		<p>
+		<a href="http://localhost:5173${route('/auth/reset-password')}?token=${resetToken}" style="color: #337ab7; text-decoration: none;">Reset your password</a>
+		</p>
+
+		<p>If you need help or have any questions, please contact our support team. We're here to help!</p>
+	</div>
+	`;
+
+	return sendEmail({
+		email,
+		subject: 'Password Reset Request',
+		htmlContent
+	});
+};
+
 export const generateEmailVerificationCode = async (userId: string, email: string) => {
 	const code = generateRandomString(EMAIL_VERIFICATION_CODE_LENGTH, alphabet('0-9'));
 
@@ -94,22 +157,6 @@ export const generateEmailVerificationCode = async (userId: string, email: strin
 	});
 
 	return code;
-};
-
-export const sendEmailVerificationCode = async (email: string, code: string) => {
-	const { error } = await resend.emails.send({
-		from: 'Lucia V3 example <onboarding@resend.dev>',
-		to: [email],
-		subject: 'Email Verification Code',
-		html: `<p>Your email verification code is: <strong>${code}</strong></p>`
-	});
-
-	if (error) {
-		console.error({ error });
-		return { success: false, message: 'Failed to send email verification code.' };
-	}
-
-	return { success: true, message: 'Email verification code sent successfully.' };
 };
 
 export const verifyEmailVerificationCode = async (userId: string, code: string) => {
@@ -159,23 +206,4 @@ export const createPasswordResetToken = async (userId: string) => {
 	});
 
 	return tokenId;
-};
-
-export const sendPasswordResetEmail = async (email: string, resetToken: string) => {
-	const { error } = await resend.emails.send({
-		from: 'Lucia V3 example <onboarding@resend.dev>',
-		to: [email],
-		subject: 'Password Reset',
-		html: `<p>Click <a href="http://localhost:5173${route('/auth/reset-password')}/${resetToken}">here</a> to reset your password.</p>`
-	});
-
-	if (error) {
-		console.error({ error });
-		return { success: false, message: 'Failed to send password reset email.' };
-	}
-
-	return {
-		success: true,
-		message: `An email has been sent to ${email} with instructions on how to reset your password.`
-	};
 };
