06d86cf26ea5d996154520803d45ef7124b26ef1
Edwards Presume <edwardspresume@gmail.com>
2024-02-19 13:01:15 -0500

    Add password reset token verification and update page load function

--- src/routes/auth/reset-password/+page.server.ts
+++ src/routes/auth/reset-password/+page.server.ts
@@ -2,10 +2,14 @@ import type { Actions, PageServerLoad } from './$types';
 
 import { superValidate } from 'sveltekit-superforms/server';
 
+import { verifyPasswordResetToken } from '$lib/database/authUtils.server';
 import { PasswordResetZodSchema } from '$validations/AuthZodSchemas';
 
-export const load = (async () => {
+export const load = (async ({ url }) => {
+	const passwordResetToken = url.searchParams.get('token') as string;
+
 	return {
+		verifyPasswordResetTokenResult: await verifyPasswordResetToken(passwordResetToken),
 		passwordResetFormData: await superValidate(PasswordResetZodSchema)
 	};
 }) satisfies PageServerLoad;
