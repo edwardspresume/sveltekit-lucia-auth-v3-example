fe0947e5004ef10fbcfcf0ecd9783445201e4ac0
Edwards Presume <edwardspresume@gmail.com>
2024-02-18 19:11:27 -0500

    Add reset password route to ROUTES.ts

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -13,6 +13,7 @@ const PAGES = {
   "/auth/email-verification": `/auth/email-verification`,
   "/auth/login": `/auth/login`,
   "/auth/register": `/auth/register`,
+  "/auth/reset-password": `/auth/reset-password`,
   "/dashboard": `/dashboard`
 }
 
@@ -118,7 +119,7 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
 * ```
 */
 export type KIT_ROUTES = { 
-  PAGES: { '/': never, '/auth/email-verification': never, '/auth/login': never, '/auth/register': never, '/dashboard': never }
+  PAGES: { '/': never, '/auth/email-verification': never, '/auth/login': never, '/auth/register': never, '/auth/reset-password': never, '/dashboard': never }
   SERVERS: Record<string, never>
   ACTIONS: { 'verifyCode /auth/email-verification': never, 'sendNewCode /auth/email-verification': never, 'logInUser /auth/login': never, 'registerUser /auth/register': never, 'logout /dashboard': never, 'deleteAllUsers /dashboard': never }
   LINKS: Record<string, never>
