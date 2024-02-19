6695aec057ce8b7dfac04692315f71f33359a086
Edwards Presume <edwardspresume@gmail.com>
2024-02-19 08:04:19 -0500

    Refactor login page to include password reset functionality

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,30 +1,61 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+
 	import { toast } from 'svelte-sonner';
 	import { superForm } from 'sveltekit-superforms/client';
 
+	import { route } from '$lib/ROUTES';
 	import {
 		MAX_EMAIL_LENGTH,
 		MAX_PASSWORD_LENGTH,
-		UserLoginZodSchema
+		UserLoginZodSchema,
+		passwordResetEmailZodSchema
 	} from '$validations/AuthZodSchemas';
 
 	import InputField from '$components/form/InputField.svelte';
 	import SubmitButton from '$components/form/SubmitButton.svelte';
-
-	import { route } from '$lib/ROUTES';
-	import type { PageData } from './$types';
+	import { buttonVariants } from '$components/ui/button';
+	import * as Dialog from '$components/ui/dialog';
 
 	export let data: PageData;
 
-	const { enhance, form, errors, message } = superForm(data.userLoginFormData, {
+	// Form for login
+	const {
+		enhance: loginEnhance,
+		form: loginForm,
+		errors: loginErrors,
+		message: loginMessage
+	} = superForm(data.userLoginFormData, {
 		resetForm: true,
 		taintedMessage: null,
 		validators: UserLoginZodSchema,
 
 		onUpdated: () => {
-			if (!$message) return;
+			if (!$loginMessage) return;
 
-			const { alertType, alertText } = $message;
+			const { alertType, alertText } = $loginMessage;
+
+			if (alertType === 'error') {
+				toast.error(alertText);
+			}
+		}
+	});
+
+	// Form for password reset
+	const {
+		enhance: resetEnhance,
+		form: resetForm,
+		errors: resetErrors,
+		message: resetMessage
+	} = superForm(data.passwordResetEmailFormData, {
+		resetForm: true,
+		taintedMessage: null,
+		validators: passwordResetEmailZodSchema,
+
+		onUpdated: () => {
+			if (!$resetMessage) return;
+
+			const { alertType, alertText } = $resetMessage;
 
 			if (alertType === 'error') {
 				toast.error(alertText);
@@ -35,13 +66,13 @@
 
 <h1 class="mb-6 text-2xl font-bold leading-none">Login</h1>
 
-<form method="post" use:enhance action={route('logInUser /auth/login')} class="space-y-4">
+<form method="post" use:loginEnhance action={route('logInUser /auth/login')} class="space-y-4">
 	<InputField
 		type="email"
 		name="email"
 		label="Email"
-		bind:value={$form.email}
-		errorMessage={$errors.email}
+		bind:value={$loginForm.email}
+		errorMessage={$loginErrors.email}
 		maxlength={MAX_EMAIL_LENGTH}
 	/>
 
@@ -49,10 +80,44 @@
 		type="password"
 		name="password"
 		label="Password"
-		bind:value={$form.password}
-		errorMessage={$errors.password}
+		bind:value={$loginForm.password}
+		errorMessage={$loginErrors.password}
 		maxlength={MAX_PASSWORD_LENGTH}
 	/>
 
-	<SubmitButton />
+	<div class="flex justify-between">
+		<SubmitButton />
+
+		<Dialog.Root>
+			<Dialog.Trigger class={buttonVariants({ variant: 'default' })}>
+				Forgot Password?
+			</Dialog.Trigger>
+			<Dialog.Content>
+				<Dialog.Header>
+					<Dialog.Title>Password Reset</Dialog.Title>
+					<Dialog.Description>
+						Enter your email address and we'll send you a link to reset your password.
+					</Dialog.Description>
+				</Dialog.Header>
+
+				<form
+					method="post"
+					use:resetEnhance
+					action={route('sendPasswordResetEmail /auth/login')}
+					class="space-y-4"
+				>
+					<InputField
+						type="email"
+						name="email"
+						label="Email"
+						bind:value={$resetForm.email}
+						errorMessage={$resetErrors.email}
+						maxlength={MAX_EMAIL_LENGTH}
+					/>
+
+					<SubmitButton>Send Reset Link</SubmitButton>
+				</form>
+			</Dialog.Content>
+		</Dialog.Root>
+	</div>
 </form>
