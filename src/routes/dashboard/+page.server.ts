3edfeba3ef0d1da9f17d73217efcdadd88182a03
Edwards Presume <edwardspresume@gmail.com>
2024-02-20 16:26:27 -0500

    Add password reset functionality to dashboard page

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,15 +1,27 @@
 import type { Actions, PageServerLoad } from './$types';
 
+import { eq } from 'drizzle-orm';
+import { Argon2id } from 'oslo/password';
 import { redirect } from 'sveltekit-flash-message/server';
+import { message, superValidate } from 'sveltekit-superforms/client';
 
 import { route } from '$lib/ROUTES';
-import { lucia } from '$lib/database/luciaAuth.server';
-import { deleteSessionCookie } from '$lib/database/authUtils.server';
+import {
+	deleteSessionCookie,
+	passwordResetActionRateLimiter
+} from '$lib/database/authUtils.server';
+import { database } from '$lib/database/database.server';
 import { deleteAllUsers, getAllUsers } from '$lib/database/databaseUtils.server';
+import { lucia } from '$lib/database/luciaAuth.server';
+import { usersTable } from '$lib/database/schema';
+import type { AlertMessageType } from '$lib/types';
 import { LOGIN_ROUTE } from '$lib/utils/navLinks';
+import { PasswordResetZodSchema } from '$validations/AuthZodSchemas';
 
-export const load = (async ({ locals: { user }, cookies }) => {
-	if (!user) {
+export const load = (async (event) => {
+	const { cookies, locals } = event;
+
+	if (!locals.user) {
 		throw redirect(
 			route('/auth/login'),
 			{
@@ -20,9 +32,12 @@ export const load = (async ({ locals: { user }, cookies }) => {
 		);
 	}
 
+	await passwordResetActionRateLimiter.cookieLimiter?.preflight(event);
+
 	return {
-		loggedInUserName: user.name,
-		allUsers: await getAllUsers()
+		loggedInUserName: locals.user.name,
+		allUsers: await getAllUsers(),
+		passwordResetFormData: await superValidate(PasswordResetZodSchema)
 	};
 }) satisfies PageServerLoad;
 
@@ -35,6 +50,79 @@ export const actions: Actions = {
 		await deleteSessionCookie(lucia, cookies);
 
 		throw redirect(303, LOGIN_ROUTE);
+	},
+
+	resetPassword: async (event) => {
+		const userId = event.locals.user?.id;
+		const currentSessionId = event.locals.session?.id;
+
+		if (!userId) return;
+
+		const passwordResetFormData = await superValidate<
+			typeof PasswordResetZodSchema,
+			AlertMessageType
+		>(event.request, PasswordResetZodSchema);
+
+		if (passwordResetFormData.valid === false) {
+			return message(passwordResetFormData, {
+				alertType: 'error',
+				alertText: 'There was a problem with your submission.'
+			});
+		}
+
+		try {
+			// Check if the rate limit for password reset action has been exceeded
+			const passwordResetActionRateLimiterResult =
+				await passwordResetActionRateLimiter.check(event);
+
+			// If the rate limit has been exceeded, return an error message
+			if (passwordResetActionRateLimiterResult.limited) {
+				return message(
+					passwordResetFormData,
+					{
+						alertType: 'error',
+						alertText: `You have made too many requests and exceeded the rate limit. Please try again after ${passwordResetActionRateLimiterResult.retryAfter} seconds.`
+					},
+					{
+						status: 429
+					}
+				);
+			}
+
+			const allUserSessions = await lucia.getUserSessions(userId);
+
+			// Invalidate all user sessions except the current session for security reasons
+			for (const session of allUserSessions) {
+				if (session.id === currentSessionId) continue;
+
+				await lucia.invalidateSession(session.id);
+			}
+
+			// Hash the new password
+			const hashedPassword = await new Argon2id().hash(passwordResetFormData.data.newPassword);
+
+			await database
+				.update(usersTable)
+				.set({ password: hashedPassword })
+				.where(eq(usersTable.id, userId));
+		} catch (error) {
+			console.error('Error in resetPassword action:', error);
+			return message(
+				passwordResetFormData,
+				{
+					alertType: 'error',
+					alertText: 'There was a problem with your submission.'
+				},
+				{
+					status: 500
+				}
+			);
+		}
+
+		return message(passwordResetFormData, {
+			alertType: 'success',
+			alertText: 'Your password has been reset successfully.'
+		});
 	},
 
 	deleteAllUsers: async ({ cookies }) => {
