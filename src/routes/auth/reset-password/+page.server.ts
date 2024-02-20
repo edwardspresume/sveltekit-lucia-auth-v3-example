ab696aea916081c07bb35d35f8c7005790436788
Edwards Presume <edwardspresume@gmail.com>
2024-02-20 08:14:33 -0500

    Add password reset rate limiting and form validation

--- src/routes/auth/reset-password/+page.server.ts
+++ src/routes/auth/reset-password/+page.server.ts
@@ -1,13 +1,19 @@
 import { error } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
 
-import { verifyPasswordResetToken } from '$lib/database/authUtils.server';
+import {
+	passwordResetActionRateLimiter,
+	verifyPasswordResetToken
+} from '$lib/database/authUtils.server';
+import type { AlertMessageType } from '$lib/types';
 import { PasswordResetZodSchema } from '$validations/AuthZodSchemas';
 
-export const load = (async ({ url }) => {
-	const passwordResetToken = url.searchParams.get('token');
+export const load = (async (event) => {
+	await passwordResetActionRateLimiter.cookieLimiter?.preflight(event);
+
+	const passwordResetToken = event.url.searchParams.get('token');
 
 	if (!passwordResetToken) {
 		error(400, 'Password reset token is missing from the request.');
@@ -20,5 +26,53 @@ export const load = (async ({ url }) => {
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	resetPassword: async () => {}
+	resetPassword: async (event) => {
+		const passwordResetToken = event.url.searchParams.get('token');
+
+		if (!passwordResetToken) return;
+
+		const passwordResetFormData = await superValidate<
+			typeof PasswordResetZodSchema,
+			AlertMessageType
+		>(event.request, PasswordResetZodSchema);
+
+		if (passwordResetFormData.valid === false) {
+			return message(passwordResetFormData, {
+				alertType: 'error',
+				alertText: 'There was a problem with your submission.'
+			});
+		}
+
+		const passwordResetActionRateLimiterResult = await passwordResetActionRateLimiter.check(event);
+
+		if (passwordResetActionRateLimiterResult.limited) {
+			return message(
+				passwordResetFormData,
+				{
+					alertType: 'error',
+					alertText: `You have made too many requests and exceeded the rate limit. Please try again after ${passwordResetActionRateLimiterResult.retryAfter} seconds.`
+				},
+				{
+					status: 429
+				}
+			);
+		}
+
+		const verifyPasswordResetTokenResult = await verifyPasswordResetToken(passwordResetToken);
+
+		if (!verifyPasswordResetTokenResult.success) {
+			return message(
+				passwordResetFormData,
+				{
+					alertType: 'error',
+					alertText: verifyPasswordResetTokenResult.message
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		console.log('Password reset form data:', passwordResetFormData);
+	}
 };
