d0b4d237ae1297396d1ed34089d55defeffb550d
Edwards Presume <edwardspresume@gmail.com>
2024-02-24 14:09:07 -0500

    added comments

--- src/routes/auth/oauth/github/+server.ts
+++ src/routes/auth/oauth/github/+server.ts
@@ -7,16 +7,21 @@ import { GITHUB_OAUTH_STATE_COOKIE_NAME } from '$lib/database/authUtils.server';
 import { githubOauth } from '$lib/database/luciaAuth.server';
 
 export const GET: RequestHandler = async ({ cookies }) => {
+	// Generate a unique state value for the OAuth  process
 	const state = generateState();
+
+	// Create the GitHub OAuth authorization URL using the state value
 	const url = await githubOauth.createAuthorizationURL(state);
 
+	// Set a cookie with the state value, to be used for CSRF protection
 	cookies.set(GITHUB_OAUTH_STATE_COOKIE_NAME, state, {
-		path: '/',
-		secure: import.meta.env.PROD,
-		httpOnly: true,
-		maxAge: 60 * 10, // 10 minutes
-		sameSite: 'lax'
+		path: '/', // The cookie will be accessible on all paths
+		secure: import.meta.env.PROD, // The cookie will be sent over HTTPS if in production
+		httpOnly: true, // The cookie cannot be accessed through client-side script
+		maxAge: 60 * 10, // The cookie will expire after 10 minutes
+		sameSite: 'lax' // The cookie will only be sent with same-site requests or top-level navigations
 	});
 
+	// Redirect the user to the GitHub OAuth authorization URL
 	redirect(302, url);
 };
