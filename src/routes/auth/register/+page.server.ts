8ea372d0361a4862b6f657e96022dd611de23a8a
Edwards Presume <edwardspresume@gmail.com>
2024-02-24 23:52:27 -0500

    Remove unused authMethod variable in register page server code

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -52,7 +52,6 @@ export const actions: Actions = {
 			await insertNewUser({
 				id: userId,
 				name: registerUserFormData.data.name,
-				authMethod: 'email',
 				email: userEmail,
 				isEmailVerified: false,
 				password: hashedPassword
