887a8c6f1bf9fa2f13086cea29226d7aaccc17ae
Edwards Presume <edwardspresume@gmail.com>
2024-02-24 22:17:06 -0500

    Add OauthButtonLinks component

---
+++ src/lib/components/OauthButtonLinks.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { route } from '$lib/ROUTES';
+
+	import Button from './ui/button/button.svelte';
+</script>
+
+<div class="flex flex-wrap gap-4">
+	<Button class="flex-grow gap-2 font-bold " href={route('GET /auth/google')}>
+		<iconify-icon icon="mdi:google" width="25"></iconify-icon>
+		<span>Google</span>
+	</Button>
+
+	<Button class="flex-grow gap-2 font-bold " href={route('GET /auth/github')}>
+		<iconify-icon icon="mdi:github" width="25"></iconify-icon>
+		<span>Github</span>
+	</Button>
+</div>
