75138c1e7e3e86c86185f9845aee52dcfd12b580
Edwards Presume <edwardspresume@gmail.com>
2024-02-24 20:41:34 -0500

    Add Google OAuth support to luciaAuth.server.ts

--- src/lib/database/luciaAuth.server.ts
+++ src/lib/database/luciaAuth.server.ts
@@ -1,8 +1,13 @@
 import { dev } from '$app/environment';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+import {
+	GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET
+} from '$env/static/private';
 
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
-import { GitHub } from 'arctic';
+import { GitHub, Google } from 'arctic';
 import { Lucia } from 'lucia';
 
 import { database } from './database.server';
@@ -10,7 +15,10 @@ import { usersSessionsTable, usersTable } from './schema';
 
 const dbAdapter = new DrizzleSQLiteAdapter(database, usersSessionsTable, usersTable);
 
+const googleRedirectUrl = `http://localhost:5173/auth/google/callback`;
+
 export const githubOauth = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
+export const googleOauth = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, googleRedirectUrl);
 
 export const lucia = new Lucia(dbAdapter, {
 	sessionCookie: {
