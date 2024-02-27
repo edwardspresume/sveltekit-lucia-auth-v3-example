e9f94483386420fc612249824258058fef668253
Edwards Presume <edwardspresume@gmail.com>
2024-02-27 14:41:24 -0500

    Refactor user login authentication logic

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -52,10 +52,28 @@ export const actions: Actions = {
 			return setError(userLoginFormData, 'email', NO_REGISTERED_ACCOUNT_ERROR_MESSAGE);
 		}
 
-		const isPasswordValid = await new Argon2id().verify(
-			existingUser.password,
-			userLoginFormData.data.password
-		);
+		let isPasswordValid = false;
+
+		// If the user has a password, it means they registered with email
+		if (existingUser.authMethods.includes('email') && existingUser.password) {
+			isPasswordValid = await new Argon2id().verify(
+				existingUser.password,
+				userLoginFormData.data.password
+			);
+		} else {
+			// If the user doesn't have a password, it means they registered with OAuth
+			return message(
+				userLoginFormData,
+				{
+					alertType: 'error',
+					alertText:
+						'You registered with an OAuth provider. Please use the appropriate login method.'
+				},
+				{
+					status: 403 // This status code indicates that the server understood the request, but it refuses to authorize it because the user registered with OAuth
+				}
+			);
+		}
 
 		if (!isPasswordValid) {
 			return setError(userLoginFormData, 'password', 'Incorrect password');
@@ -142,7 +160,7 @@ export const actions: Actions = {
 					},
 
 					{
-						status: 500 // Internal Server Error 
+						status: 500 // Internal Server Error
 					}
 				);
 			}
